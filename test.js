4493d2768cfdad8f5e88cc3c779da7c030465b80
Kenan Sulayman <kenan@sly.mn>
2019-09-15 16:03:18 +0200

    Add test for 6 and 10 dimensions

--- test.js
+++ test.js
@@ -27,3 +27,33 @@
     console.log('4 dim', x.nearest([1, 2, 3, 4], 6));
     console.log('4 dim', x.within([1, 2, 3, 4], 40));
 }
+
+{
+    const x = new (require("./pkg").KdTree)(6);
+
+    x.add([1, 1, 7, 1, 1, 1], 1);
+    x.add([3, 4, 8, 8, 3, 4], 3);
+    x.add([4, 9, 2, 6, 4, 9], 5);
+    x.add([7, 1, 1, 1, 1, 1], 7);
+    x.add([8, 8, 3, 4, 3, 4], 23);
+    x.add([2, 6, 4, 9, 4, 9], 8);
+
+    console.log('6 dim', x.size());
+    console.log('6 dim', x.nearest([1, 2, 3, 4, 5, 6], 6));
+    console.log('6 dim', x.within([1, 2, 3, 4, 5, 6], 60));
+}
+
+{
+    const x = new (require("./pkg").KdTree)(10);
+
+    x.add([1, 1, 7, 1, 1, 1, 7, 1, 1, 1], 1);
+    x.add([3, 4, 8, 8, 3, 4, 8, 8, 3, 4], 3);
+    x.add([4, 9, 2, 6, 4, 9, 2, 6, 4, 9], 5);
+    x.add([7, 1, 1, 1, 1, 1, 1, 1, 7, 1], 7);
+    x.add([8, 8, 3, 4, 3, 4, 3, 4, 8, 8], 23);
+    x.add([2, 6, 4, 9, 4, 9, 4, 9, 2, 6], 8);
+
+    console.log('10 dim', x.size());
+    console.log('10 dim', x.nearest([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6));
+    console.log('10 dim', x.within([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 129));
+}
